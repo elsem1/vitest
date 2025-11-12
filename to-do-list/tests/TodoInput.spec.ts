c42a3c9e0c4b0437277a616fa0bda82e46a32d97
elwin <elwinsemper@mail.com>
2025-11-12 14:25:30 +0100

    opdracht 16

--- to-do-list/tests/TodoInput.spec.ts
+++ to-do-list/tests/TodoInput.spec.ts
@@ -1,19 +1,26 @@
 import TodoInput from '../src/components/TodoInput.vue';
 import {shallowMount} from '@vue/test-utils';
 
-describe('TodoInput', () => {
-    it('should render an input field where you can type a task you want to add', () => {
+describe('TodoInput', () => {    
+    it('should render an input field', () => {
         // Arrange 
         const wrapper = shallowMount(TodoInput);
-        const newTodo = 'A new task';
         const input = wrapper.getByDataTest('todo-input');
 
-        // Act 
-        input.setValue(newTodo);
-
         // Assert
-        expect(wrapper.getByDataTest('todo-input').exists()).toBe(true);
-        expect(input.element.value).toContain(newTodo);        
+        expect(input.exists()).toBe(true);
+    });
+
+    it('should update input value when typing', async () => {
+        // Arrange
+        const wrapper = shallowMount(TodoInput);
+        const input = wrapper.getByDataTest('todo-input');
+        
+        // Act
+        await input.setValue('A new task');        
+        
+        // Assert
+        expect((input.element as HTMLInputElement).value).toBe('A new task');
     });
 
     it('should render a submit button that handles an emit when form is submitted', async () => {
@@ -30,6 +37,20 @@ describe('TodoInput', () => {
         expect(wrapper.emitted('add-todo')?.[0]).toEqual(['Belangrijke taak']);
     });
 
+    it('should clear input field after submit', async () => {
+        // Arrange
+        const wrapper = shallowMount(TodoInput);
+        const input = wrapper.getByDataTest('todo-input');
+        const form = wrapper.getByDataTest('todo-form');
+
+        // Act
+        await input.setValue('Belangrijke taak');
+        await form.trigger('submit');
+
+        // Arrange
+        expect((input.element as HTMLInputElement).value).toBe('');
+    });
+
     it('should disable button when input is empty', () => {
         // Arrange 
         const wrapper = shallowMount(TodoInput);
@@ -37,6 +58,18 @@ describe('TodoInput', () => {
 
         // Assert 
         expect((button.element as HTMLButtonElement).disabled).toBe(true);
+    });
+
+    it('should not emit event when input is empty', async () => {
+        // Arrange
+        const wrapper = shallowMount(TodoInput);
+        const form = wrapper.getByDataTest('todo-form');
+
+        // Act
+        await form.trigger('submit');
+        
+        // Assert
+        expect(wrapper.emitted('add-todo')).toBeUndefined();
     });
 
     it('should disable button when input is only whitespace', async () => {
